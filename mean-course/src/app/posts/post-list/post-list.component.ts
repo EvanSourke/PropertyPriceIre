bb37360abb92548bd42d613c3f602d3ffc2d3aca
EvanSourke <evansourke12@gmail.com>
2022-11-24 15:45:23 +0000

    Restarting proj up to creating expansion panel

---
+++ mean-course/src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+
+export class PostListComponent {
+  posts = [
+    {title: 'First Post', content: 'This is the first post\'s content' },
+    {title: 'Second Post', content: 'This is the Second post\'s content' },
+    {title: 'Third Post', content: 'This is the third post\'s content' },
+  ];
+
+
+}
