1a4c3db0100c7ec6083f4f31a65d5eb249e84a2c
EvanSourke <evansourke12@gmail.com>
2023-01-30 16:25:24 +0000

    Added angular routes for separate pages

--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
+import { AppRoutingModule } from './app-routing.module';
 
 
 
@@ -25,6 +26,7 @@ import { PostListComponent } from './posts/post-list/post-list.component';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
     MatInputModule,
--- mean-course/src/app/header/header.component.ts
+++ mean-course/src/app/header/header.component.ts
@@ -3,7 +3,8 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html'
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
 })
 
 
