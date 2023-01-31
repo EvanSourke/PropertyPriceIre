9e5d2c9c17413e1d092a07f52f2238dc4a29ef0d
EvanSourke <evansourke12@gmail.com>
2023-01-31 17:32:35 +0000

    Created house search component, with count and Property type inputs

---
+++ mean-course/src/app/Houses/house-search/house-search.component.ts
@@ -0,0 +1,87 @@
+import { Component, OnInit } from "@angular/core";
+import { FormControl } from "@angular/forms";
+import { Observable } from "rxjs";
+import {startWith, map} from 'rxjs/operators';
+
+
+
+
+@Component({
+  selector: 'app-house-search',
+  templateUrl: './house-search.component.html',
+  styleUrls: ['./house-search.component.css']
+})
+
+export class HouseSearchComponent implements OnInit {
+
+  control = new FormControl('');
+  counties: string[] = [
+  'Antrim',
+  'Armagh',
+  'Carlow',
+  'Cavan',
+  'Clare',
+  'Cork',
+  'Derry',
+  'Donegal',
+  'Down',
+  'Dublin',
+  'Fermanagh',
+  'Galway',
+  'Kerry',
+  'Kildare',
+  'Kilkenny',
+  'Laois',
+  'Leitrim',
+  'Limerick',
+  'Longford',
+  'Louth',
+  'Mayo',
+  'Meath',
+  'Monaghan',
+  'Offaly',
+  'Roscommon',
+  'Sligo',
+  'Tipperary',
+  'Tyrone',
+  'Waterford',
+  'Westmeath',
+  'Wexford',
+  'Wicklow'];
+
+  properties: string[] = ['New Dwelling house /Apartment','Second-Hand Dwelling house /Apartment'];
+
+
+
+  filteredCounties: Observable<string[]>;
+  filteredProperties: Observable<string[]>;
+
+  ngOnInit() {
+    this.filteredCounties = this.control.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filterCounties(value || '')),
+    );
+
+    this.filteredProperties = this.control.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filterProperties(value || '')),
+    );
+
+
+  }
+
+  private _filterCounties(value: string): string[] {
+    const filterValue = this._normalizeValue(value);
+    return this.counties.filter(street => this._normalizeValue(street).includes(filterValue));
+  }
+
+  private _filterProperties(value: string): string[] {
+    const filterValue = this._normalizeValue(value);
+    return this.properties.filter(street => this._normalizeValue(street).includes(filterValue));
+  }
+
+  private _normalizeValue(value: string): string {
+    return value.toLowerCase().replace(/\s/g, '');
+  }
+
+}
--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -1,33 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatInputModule } from '@angular/material/input';
+import { MatInputModule} from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
-import { PostCreateComponent } from './posts/post-create/post-create.component';
+//import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
-import { PostListComponent } from './posts/post-list/post-list.component';
+//import { PostListComponent } from './posts/post-list/post-list.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HouseSearchComponent } from './Houses/house-search/house-search.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent,
     HeaderComponent,
-    PostListComponent
+    HouseSearchComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule,
     BrowserAnimationsModule,
     MatInputModule,
     MatButtonModule,
