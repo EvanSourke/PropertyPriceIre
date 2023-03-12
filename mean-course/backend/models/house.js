e06356d359a2e86a6581df3f8c1cd28c86de0958
EvanSourke <79155403+EvanSourke@users.noreply.github.com>
2023-03-12 18:54:26 +0000

    Trying to fix http errors for get request

--- mean-course/backend/models/house.js
+++ mean-course/backend/models/house.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 const houseSchema = mongoose.Schema({
-  dateOfSale: {type: String, required: true },
+  dateOfSale: {type: Date, required: true },
   Address: { type: String, required: true },
   County: { type: String, required: true },
   Price: { type: Number, required: true },
